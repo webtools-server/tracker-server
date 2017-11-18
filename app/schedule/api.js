e0b15baaa768eeb0df5c19308d9d0d7b495ee341
canye <canye.wu@jyblife.com>
2017-11-18 16:54:13 +0800

    feat: 修改定时任务

--- app/schedule/api.js
+++ app/schedule/api.js
@@ -2,13 +2,15 @@
  * 接口数据定时任务
  */
 
+const { TRACKER_TYPE } = require('../common/enum');
+
 exports.schedule = {
   cron: '0 0 */2 * * *', // 2个小时执行一次
   type: 'all',
   immediate: process.env.NODE_ENV === 'production' // production环境下应用启动的时候，立即执行
 };
 
-exports.task = function () {
-  console.log('api schedule');
-  // this.ctx.logger.info('all&&cron');
+exports.task = function* () {
+  this.ctx.logger.info('获取API错误数据');
+  yield this.ctx.service.tracker.getAllData(TRACKER_TYPE.API);
 };
