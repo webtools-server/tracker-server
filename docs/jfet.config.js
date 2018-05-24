6852f87d300a68b409fc50988619cad1f27564bf
canye <canye.wu@jyblife.com>
2018-05-24 11:52:26 +0800

    docs: 添加文档

---
+++ docs/jfet.config.js
@@ -0,0 +1,15 @@
+/**
+ * jfet配置
+ */
+
+module.exports = {
+  doc() {
+    return {
+      name: 'tracker',
+      title: '前端监控系统',
+      desc: '前端监控系统文档，包括上报规范，接入方式，告警配置，问题定位等内容。',
+      token: '21232F297A57A5A743894A0E4A801FC3',
+      uploadUrl: 'http://doc.fe.jyb.com/api/upload'
+    };
+  }
+};
